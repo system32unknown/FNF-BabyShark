1f58aee1ca97592663c7788e2c6fb029d6a3394d
Altertoriel <op58274@gmail.com>
2023-02-19 02:01:21 +0900

    mass :skull:

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -3159,29 +3159,26 @@ class PlayState extends MusicBeatState
 			health = healthMax;
 
 		if (healthBar.percent < 20) {
-			if (playerIcon.animated) {
+			if (iconP1.animated) {
 				iconP1.animation.play('dead');
+			} else iconP1.setState(1);
+			if (iconP2.animated) {
 				iconP2.animation.play('win');
-			} else {
-				iconP1.setState(1);
-				iconP2.setState(2);			
-			}
+			} else iconP2.setState(2);
 		} else if (healthBar.percent > 80) {
-			if (playerIcon.animated) {
+			if (iconP1.animated) {
 				iconP1.animation.play('win');
+			} else iconP1.setState(2);
+			if (iconP2.animated) {
 				iconP2.animation.play('dead');
-			} else {
-				iconP1.setState(2);
-				iconP2.setState(1);
-			}
+			} else iconP2.setState(1);
 		} else {
-			if (playerIcon.animated) {
+			if (iconP1.animated) {
 				iconP1.animation.play('idle');
+			} else iconP1.setState(0);
+			if (iconP2.animated) {
 				iconP2.animation.play('idle');
-			} else {
-				iconP1.setState(0);
-				iconP2.setState(0);
-			}
+			} else iconP2.setState(0);
 		}
 
 		if (FlxG.keys.anyJustPressed(debugKeysCharacter) && !endingSong && !inCutscene) {
