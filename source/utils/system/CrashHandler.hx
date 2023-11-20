e609566d315fa74cf2b6f08b5fedd2e633af9efe
Altertoriel <op58274@gmail.com>
2023-11-20 16:47:58 +0900

    ay

--- source/utils/system/CrashHandler.hx
+++ source/utils/system/CrashHandler.hx
@@ -28,11 +28,11 @@ class CrashHandler {
 			message = '${err.text}';
 		} else message = try Std.string(e) catch(_:haxe.Exception) "Unknown";
 
-		var errMsg:String = "";
-		final callStack:Array<StackItem> = CallStack.exceptionStack(true);
 		var dateNow:String = Date.now().toString().replace(" ", "_").replace(":", "'");
-
 		final path = './crash/PsychEngine_$dateNow.txt';
+
+		final callStack:Array<StackItem> = CallStack.exceptionStack(true);
+		var errMsg:String = "";
 
 		for (stackItem in callStack) {
 			switch (stackItem) {
@@ -40,7 +40,7 @@ class CrashHandler {
 				case Module(c): errMsg += 'Module ${c}\n';
 				case FilePos(p, file, line, _):
 					switch(p) {
-						case Method(cla, func): errMsg += '$file ${cla.split(".")[cla.split(".").length - 1]}.$func() - (line $line)\n';
+						case Method(cla, func): errMsg += '[$file] ${cla.split(".")[cla.split(".").length - 1]}.$func() - (line $line)\n';
 						case _: errMsg += '$file (line $line)\n';
 					}
 				case LocalFunction(v): errMsg += 'Local Function ${v}\n';
@@ -53,12 +53,11 @@ class CrashHandler {
 		e.stopPropagation();
 		e.stopImmediatePropagation();
 
-		errMsg += '\nUncaught Error: $message\nPlease report this error to the GitHub page: https://github.com/system32unknown/FNF-BabyShark\n\n Crash Handler written by: sqirra-rng\nCustom Crash Handler by: Altertoriel';
+		errMsg += '\nUncaught Error: $message\nPlease report this error to the GitHub page: https://github.com/system32unknown/FNF-BabyShark\n\nCrash Handler written by: sqirra-rng\nCustom Crash Handler by: Altertoriel';
 
 		if (!FileSystem.exists("./crash/"))
 			FileSystem.createDirectory("./crash/");
-
-		File.saveContent(path, errMsg + "\n");
+		File.saveContent(path, 'errMsg\n');
 
 		Sys.println(errMsg);
 		Sys.println("Crash dump saved in " + Path.normalize(path));
