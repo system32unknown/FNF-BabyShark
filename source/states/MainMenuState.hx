4cf290609388df311d23b6d7956ff7851494af02
Altertoriel <op58274@gmail.com>
2024-04-02 00:47:18 +0900

    done

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -108,9 +108,6 @@ class MainMenuState extends MusicBeatState {
 		add(version);
 		changeItem();
 
-		var leDate:Date = Date.now();
-		if (leDate.getMonth() == 3 && leDate.getDate() == 1)
-
 		super.create();
 		FlxG.camera.follow(camFollow, null, 9);
 	}
