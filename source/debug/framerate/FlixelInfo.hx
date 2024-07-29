e240960b5b7be711f96d114989187acccaa4992e
Altertoriel <op58274@gmail.com>
2024-07-30 04:27:46 +0900

    ok

--- source/debug/framerate/FlixelInfo.hx
+++ source/debug/framerate/FlixelInfo.hx
@@ -9,7 +9,8 @@ class FlixelInfo extends FramerateCategory {
 		if (alpha <= 0.05) return;
 
 		_text = 'State: ${Type.getClassName(Type.getClass(FlxG.state))}';
-		_text += '\nSubstate: ' + (FlxG.state.subState != null ? Type.getClassName(Type.getClass(FlxG.state.subState)) : 'None');
+		if (FlxG.state.subState != null) _text += '\nSub: ${Type.getClassName(Type.getClass(FlxG.state.subState))}';
+		_text += '\nObjs:${FlxG.state.members.length}, Cams:${FlxG.cameras.list.length}, Snds:${FlxG.sound.list.length}';
 
 		if (this.text.text != _text) this.text.text = _text;
 		super.__enterFrame(t);
