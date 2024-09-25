0f31bb884493a75b9271a9d4865dea04ff64ff64
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2024-09-25 09:51:26 +0900

    ok

--- source/debug/CrashHandler.hx
+++ source/debug/CrashHandler.hx
@@ -1,7 +1,6 @@
 package debug;
 
 //crash handler stuff
-import haxe.CallStack;
 import openfl.events.UncaughtErrorEvent;
 import openfl.events.ErrorEvent;
 import openfl.errors.Error;
@@ -29,7 +28,7 @@ class CrashHandler {
 	static function onError(message:String):Void {
 		final path:String = './crash/${FlxG.stage.application.meta.get('file')}_${Date.now().toString().replace(" ", "_").replace(":", "'")}.txt';
 		
-		var errMsg:String = CallStack.toString(CallStack.exceptionStack(true)).trim();
+		var errMsg:String = getError();
 		errMsg += '\nPlatform: ${System.platformLabel} ${System.platformVersion}';
 		errMsg += '\nFlixel Current State: ${Type.getClassName(Type.getClass(FlxG.state))}';
 		errMsg += '\nUncaught Error: $message\nPlease report this error to the GitHub page: https://github.com/system32unknown/FNF-BabyShark\n\nCustom Crash Handler written by: sqirra-rng and Codename Engine Team and Altertoriel';
@@ -45,5 +44,25 @@ class CrashHandler {
 		NativeUtil.showMessageBox("Alter Engine: Error!", errMsg, MSG_ERROR);
 		#if DISCORD_ALLOWED DiscordClient.shutdown(); #end
 		System.exit(1);
+	}
+
+	static function getError():String {
+		var error:String = "";
+		for (stackItem in haxe.CallStack.exceptionStack(true)) {
+			switch (stackItem) {
+				case FilePos(p, file, line, column):
+					switch(p) {
+						case Method(cla, func): error += '[$file] ${cla.split(".")[cla.split(".").length - 1]}.$func() - (line $line)';
+						case _: error += '$file (line $line)';
+					}
+					if (column != null) error += ':$column';
+				case CFunction: error += "Non-Haxe (C) Function";
+				case Module(c): error += 'Module $c';
+				case Method(cl, m): error += '$cl - $m';
+				case LocalFunction(v): error += 'Local Function $v';
+			}
+			error += '\n';
+		}
+		return error;
 	}
 }
