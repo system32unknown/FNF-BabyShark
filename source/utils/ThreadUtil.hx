8af21cf6938973e3ee44c1692ef89d311e1ed7e9
Altertoriel <op58274@gmail.com>
2025-04-23 00:15:34 +0900

    done

--- source/utils/ThreadUtil.hx
+++ source/utils/ThreadUtil.hx
@@ -2,24 +2,47 @@ package utils;
 
 import sys.thread.Thread;
 
+/**
+ * Utility class to handle multithreading logic for the game.
+ * Provides functionality for initializing threads, executing functions asynchronously,
+ * and safely creating threads with error handling and optional auto-restart.
+ */
 class ThreadUtil {
+	/**
+	 * Array storing all threads used for game logic execution.
+	 */
 	public static var gameThreads:Array<Thread> = [];
 
+	/**
+	 * Initializes a set of game threads with event loops for asynchronous task handling.
+	 * This setup creates 4 threads that can be used to distribute workload.
+	 */
 	public static function init() {
 		for (i in 0...4) gameThreads.push(Thread.createWithEventLoop(() -> Thread.current().events.promise()));
 	}
 
+	/**
+	 * Internal counter used to cycle through threads in a round-robin fashion.
+	 */
 	static var __threadCycle:Int = 0;
 
+	/**
+	 * Executes a given function asynchronously on one of the initialized threads.
+	 * Threads are chosen using round-robin scheduling.
+	 * @param func The function to be executed asynchronously.
+	 */
 	public static function execAsync(func:Void->Void) {
 		var thread:Thread = gameThreads[(__threadCycle++) % gameThreads.length];
 		thread.events.run(func);
 	}
 
 	/**
-	 * Creates a new Thread with an error handler.
-	 * @param func Function to execute
-	 * @param autoRestart Whenever the thread should auto restart itself after crashing.
+	 * Creates and starts a new thread to safely execute a function, optionally restarting the thread upon errors.
+	 * Useful for resilient background tasks where failure recovery is needed.
+	 *
+	 * @param func The function to be executed in the new thread.
+	 * @param autoRestart If true, the thread will restart itself after crashing.
+	 * @return The created thread.
 	 */
 	public static function createSafe(func:Void->Void, autoRestart:Bool = false) {
 		return Thread.create(function() {
